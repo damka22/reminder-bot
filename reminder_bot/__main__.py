5c4c2ef5624d387c36932686d78baa770ef11350
damka22 <damirzar07@mail.ru>
2025-08-17 19:23:49 +0500

    add keyboarderror to beautiful Bot Exit

--- reminder_bot/__main__.py
+++ reminder_bot/__main__.py
@@ -5,4 +5,7 @@ import asyncio
 from .run import main
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Bot exit")
